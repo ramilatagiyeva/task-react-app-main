62a0dcab3d6a4961a0fa56e0c22ca57f718f60cc
ramilatagiyeva <tagiyevaramila02@gmail.com>
2024-12-25 16:33:03 +0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './Header';
+import FirstSection from './FirstSection';
+import SecondSection from './SecondSection';
+import ThreeSection from './ThreeSection';
+import Footer from './Footer';
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <FirstSection />
+      <SecondSection />
+      <ThreeSection />
+      <Footer />
+    </div>
+  );
+}
+export default App;
